fafa2dbad0bc73224d10a059a48052577cc3a5a5
UPIMAN73 <joshcalzadi@gmail.com>
2023-12-09 17:32:59 -0500

    Map Shuffle Reduce

--- Project 2/main.py
+++ Project 2/main.py
@@ -9,10 +9,11 @@ __status__ = "Development"
 #############
 # Libraries #
 #############
+from time import time as now
 import requests
+import datetime
 import json
 import yaml
-import sys
 import re
 
 
@@ -119,7 +120,7 @@ def Shuffle(inputStorage):
                 result[item[1]].append(item)
             else:
                 result[item[1]] = [item]
-    print(result)
+    
     # Return
     return result
 
@@ -127,12 +128,12 @@ def Shuffle(inputStorage):
 def Reduce(shuffledData, header):
     # Variable definitions
     result = {}
-    # item = {"word" : ((ID1, COUNT_1), (ID2, COUNT_2), ...)} # Final Result
 
     # Reducing filter
-    for word, items in shuffledData:
+    for word in shuffledData.keys():
         temp = {}
         result[word] = []
+        items = shuffledData[word] # Easier to manage with memory
 
         # First step of reduction
         for item in items:
@@ -144,8 +145,8 @@ def Reduce(shuffledData, header):
         
         # Second step of reduction
         # Convert the temp variable into a proper tupule
-        for id, value in temp:
-            result[word].append(tuple(id, value))
+        for id in temp.keys():
+            result[word].append((id, temp[id]))
     return result
 
 
@@ -157,7 +158,17 @@ storageBlocks = 200
 storage = { "blocks" : storageBlocks,  # Number of buckets inside of the storage container
             "max-size" : storageBlockSize, # Maximum number of items inside of the bucket
             "container" : [{"size" : 0, "data" : []} for i in range(0, storageBlocks)] } # Initilize the storage container
+
+# Output Data
 Output = {}
+shuffledStorage = {}
+
+# Stats Data
+mapTimings = []
+shuffleTiming = 0
+reduceTiming = 0
+initTime = 0
+finalTime = 0
 
 
 ###########################
@@ -180,24 +191,41 @@ try:
         # REST API Request to parse and load data into proper order
         data = getRawContent(book[1])
         resourceHeaders.append(book[0])
-        Map(data, (ind + 1), storage=storage, allocSize=100)
 
+        # Map stage
+        initTime = now()
+        storage = Map(data, (ind + 1), storage=storage, allocSize=100)
+        finalTime = now()
+        mapTimings.append(finalTime - initTime)
 
 except KeyboardInterrupt:
     print("Keyboard Interrupt Detected!")
 
 finally:
+    # Shuffle stage
+    initTime = now()
     shuffledStorage = Shuffle(storage)
+    finalTime = now()
+    shuffleTiming = finalTime - initTime
+
+    # Reduce stage
+    initTime = now()
     Output = Reduce(shuffledStorage, resourceHeaders)
+    finalTime = now()
+    reduceTiming = finalTime - initTime
     
     # Save storage container into a proper format
     with open("Output-Storage-Container.json", "w") as outputFile:
         json.dump(storage, outputFile, indent=2)
 
-    # Save storage container into a proper format
+    # # Save storage container into a proper format
     with open("Output-Shuffle.json", "w") as outputFile:
         json.dump(shuffledStorage, outputFile, indent=2)
     
-    # Map Reduced version of a word counter processed and dumped into a JSON file
+    # # Map Reduced version of a word counter processed and dumped into a JSON file
     with open("Output-Word-Count.json", "w") as outputFile:
         json.dump(Output, outputFile, indent=2)
+    
+    # Get output statistics on the algorithm run-times
+    with open("Algorithm-Stats.json", "w") as outputFile:
+        json.dump({"Units" : "s", "Map-Timings" : mapTimings, "Map" : sum(mapTimings)/len(mapTimings), "Shuffle" : shuffleTiming, "Reduce" : reduceTiming}, outputFile, indent=4)
